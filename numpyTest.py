176f320a5e696b2daf8f15b2a676d87754e45eb6
chigbolu <e.chigbolu@se15.qmul.ac.uk>
2016-11-24 15:57:32 +0000

    Practsing kmeans , added code for creating graphs, calculated single ticket instances

--- numpyTest.py
+++ numpyTest.py
@@ -1,8 +1,19 @@
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
 import csv as csv
 import numpy as np
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-trainData = csv.reader(open('./train2.csv','rb'))
+
+
+trainData = csv.reader(open('./train.csv','rb'))
 header = trainData.next();
 
 data = []
@@ -144,7 +155,8 @@ for row in travellingThirdFromQ:
 #get the price of a single ticket embark S
 for row in travellingFirstFromS:
 	ticket = row[8]
-	if(float(row[9]) != 0.):
+	
+	if(float(row[9]) != 0. ):
 		price = float(row[9])
         if (ticket in dictFirstS):
                 ticketCostsFirstS.append(price/dictFirstS.get(ticket))
@@ -219,15 +231,23 @@ for row in travellingThirdFromQ:
 		if (ticket in dictThirdQ):
                 	ticketCostsThirdQ.append(price/dictThirdQ.get(ticket))
 
-
-print(ticketCostsFirstS)
-print(ticketCostsSecondS)
-print(ticketCostsThirdS)
+#print("First S")
+#print(sorted(ticketCostsFirstS))
+#print("Second S")
+#print(ticketCostsSecondS)
+#print("Third S")
+#print(ticketCostsThirdS)
+#print("First C")
 #print(ticketCostsFirstC )
+#print("Second C")
 #print(ticketCostsSecondC)
+#print("Third C")
 #print(ticketCostsThirdC)
+#print("First Q")
 #print(ticketCostsFirstQ )
+#print("Second Q")
 #print(ticketCostsSecondQ )
+#print("Third C")
 #print(ticketCostsThirdQ )
 
 ticketAverageCosts = []
@@ -251,7 +271,7 @@ ticketAverageCosts.append(sum(ticketCostsSecondQ)/len(ticketCostsSecondQ))
 
 ticketAverageCosts.append(sum(ticketCostsThirdQ)/len(ticketCostsThirdQ))
 
-print(ticketAverageCosts)
+#print(ticketAverageCosts)
 
 #stefan
 #calculate family sizes
@@ -275,54 +295,175 @@ for key,value in dictFamilies.iteritems():
 
 #get the price of a single ticket embark S
 
-singleTicketS = []
+singleTicket1S = []
 for row in travellingFirstFromS:
 	if(row[8] in dictFirstS):
 		if(dictFirstS[row[8]] == 1):
-			fare = row[9]	
-			singleTicketS.append(fare)
+			fare = float(row[9])
+			singleTicket1S.append(fare)
+
+print("Single 1s")
+print(sum(singleTicket1S)/len(singleTicket1S))
+
+singleTicket1C = []
+for row in travellingFirstFromC:
+	if(row[8] in dictFirstC):
+		if(dictFirstC[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket1C.append(fare)
+
+print("Single 1c")
+print(sum(singleTicket1C)/len(singleTicket1C))
+
+singleTicket1Q = []
+for row in travellingFirstFromQ:
+	if(row[8] in dictFirstQ):
+		if(dictFirstQ[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket1Q.append(fare)
+
+#print("Single 1q")
+#print(sum(singleTicket1Q)/len(singleTicket1Q))
+
+singleTicket2S = []
+
+for row in travellingSecondFromS:
+	fA = []
+	if(row[8] in dictSecondS):
+		if(dictSecondS[row[8]] == 1):
+			if(row[5] != "" and row[9] != ""):
+				fare = float(row[9])
+				age = float(row[5])
+				fA.append(age)
+				fA.append(fare)
+				singleTicket2S.append(fA)
+			
+
+#print("Single 2s")
+#print(sum(singleTicket2S)/len(singleTicket2S))
+
+singleTicket2C = []
+for row in travellingSecondFromC:
+	if(row[8] in dictSecondC):
+		if(dictSecondC[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket2C.append(fare)
+print("Single 2C")
+print(sum(singleTicket2C)/len(singleTicket2C))
 
 
-print(singleTicketS)
+singleTicket2Q = []
+for row in travellingSecondFromQ:
+	if(row[8] in dictSecondQ):
+		if(dictSecondQ[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket2Q.append(fare)
+print("Single 2q")
+print(sum(singleTicket2Q)/len(singleTicket2Q))
+
+singleTicket3S = []
+for row in travellingThirdFromS:
+	if(row[8] in dictThirdS):
+		if(dictThirdS[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket3S.append(fare)
+
+print("Single 3s")
+print(sum(singleTicket3S)/len(singleTicket3S))
+
+singleTicket3C = []
+for row in travellingThirdFromC:
+	if(row[8] in dictThirdC):
+		if(dictThirdC[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket3C.append(fare)
+
+print("Single 3c")
+print(sum(singleTicket3C)/len(singleTicket3C))
+
+singleTicket3Q = []
+for row in travellingThirdFromQ:
+	if(row[8] in dictThirdQ):
+		if(dictThirdQ[row[8]] == 1):
+			fare = float(row[9])
+			singleTicket3Q.append(fare)
+
+print("Single 3Q")
+print(sum(singleTicket3Q)/len(singleTicket3Q))
+
+
+doubleValues = []
+
+# kmeans does not give great results for the age problem
+for row in travellingFirstFromS:
+	if(row[5] is not None and row[9] is not None):
+		ageFare = []
+		try:
+			ageFare.append(int(row[5]))
+			#ageFare.append(int(row[1]))
+			#ageFare.append(int(row[6]))
+			#ageFare.append(int(row[7]))
+			ageFare.append(float(row[9]))
+			doubleValues.append(ageFare)
+		except ValueError,e:
+			print "Error --> " ,e
+			
+
+print("Here the double values")
+print(doubleValues)
+
+kmeans = KMeans(n_clusters=10, random_state=0).fit(doubleValues)
+kmeans.labels_
+print(kmeans.cluster_centers_)
+
+
+doubleValues = []
+
+# kmeans does not give great results for the age problem
+for row in singleTicket2S:
+	ageFare = []
+	ageFare.append(int(row[0]))
+			#ageFare.append(int(row[1]))
+			#ageFare.append(int(row[6]))
+			#ageFare.append(int(row[7]))
+	ageFare.append(float(row[1]))
+	doubleValues.append(ageFare)
+			
+
+print("Here the double values")
+print(doubleValues)
+
+kmeans = KMeans(n_clusters=15, random_state=0).fit(doubleValues)
+
+print(kmeans.cluster_centers_)
+
+x = []
+y = []
+xy = []
+
+# kmeans does not give great results for the age problem
+for row in singleTicket2S:
+	ageFare = []
+	x.append(int(row[0]))
+			#ageFare.append(int(row[1]))
+			#ageFare.append(int(row[6]))
+			#ageFare.append(int(row[7]))
+	y.append(float(row[1]))
+		#	doubleValues.append(ageFare)
+		
+xy.append(y)
+print(singleTicket2S)
+
+plt.plot(x,y, 'ro')
+plt.axis([0, 90, 0, 50])
+plt.show()
+#y = np.sin(4 * np.pi * x) * np.exp(-5 * x)
+
+#plt.fill(x, y, 'r')
+#plt.grid(True)
+#plt.show()
 
 
 
 
 
-
-
-#print(travellingFirstFromC)
-
-
-
-#to do: Store familes by surname possibly
-#estimate how many members of family survive
-	#store families by second name and family size
-
-
-
-
-
-#print(ticketInstances)
-#print(travellingFirstFromC)
-
-
-#travellingFirstFromS = travellingFromS[0::,2] == "1"
-#travellingSecondFromS = travellingFromS[0::,2] == "2"
-#travellingThirdFromS = travellingFromS[0::,2] == "3"
-
-#travellingFirstFromC = travellingFromC[0::,2] == "1"
-#travellingSecondFromC = travellingFromC[0::,2] == "2"
-#travellingThirdFromC = travellingFromC[0::,2] == "3"
-
-#travellingFirstFromQ = travellingFromC[0::,2] == "1"
-#travellingSecondFromQ = travellingFromC[0::,2] == "2"
-#travellingThirdFromQ = travellingFromC[0::,2] == "3"
-
-#how to group them?
-#sort tickets
-#count the instances
-#if one - store in singles
-#if two+ stor in families
-#count out all people with fare 0
-#add float convert

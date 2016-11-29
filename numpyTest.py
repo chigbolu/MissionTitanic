9164d0f56ea45e5d9f5524e5f0fe8f82ac25d9c3
Stefan Ionascu <ionascustefan@hotmail.com>
2016-11-29 15:44:04 +0000

    WIP - replace csv column(age) with average of age per title

--- numpyTest.py
+++ numpyTest.py
@@ -4,7 +4,7 @@
 import csv as csv
 from time import time
 import pandas as pd
-
+import math as math
 titlesAverages = dict.fromkeys(['Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don',
 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Mr', 'Master', 'Ms', 'Mrs'])
 df = pd.read_csv('train.csv', header=0)
@@ -14,3 +14,19 @@ for title in titlesAverages:
     average = rows['Age'].mean()
     titlesAverages[title] = int(average)
 
+def getTitle(name):
+    for title in titlesAverages:
+        if title + '.' in name:
+            return title
+    return 'None'
+                                
+#Replace empty age column with averages
+for index,row in df.iterrows():
+    age = row['Age']
+    if(math.isnan(age)):
+        title = getTitle(row['Name'])
+        if title == 'None':
+            continue
+        average = titlesAverages[title]
+        row['Age'] = average
+df.to_csv("train.csv")

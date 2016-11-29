8399cec61aa9216726620d03725d20ae88fd3163
Stefan Ionascu <ionascustefan@hotmail.com>
2016-11-29 22:55:54 +0000

    Working - replacing empty age with average of that title

--- numpyTest.py
+++ numpyTest.py
@@ -5,10 +5,11 @@ import csv as csv
 from time import time
 import pandas as pd
 import math as math
-titlesAverages = dict.fromkeys(['Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don',
+titlesAverages = dict.fromkeys(['Miss','Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don',
 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Mr', 'Master', 'Ms', 'Mrs'])
 df = pd.read_csv('train.csv', header=0)
 #Calculate averages for each title
+#TODO: Calculate average for no titles(None returned by getTitle)
 for title in titlesAverages: 
     rows = df[df.Name.str.contains(' '+title+'.')]
     average = rows['Age'].mean()
@@ -19,14 +20,16 @@ def getTitle(name):
         if title + '.' in name:
             return title
     return 'None'
-                                
+
 #Replace empty age column with averages
 for index,row in df.iterrows():
     age = row['Age']
     if(math.isnan(age)):
         title = getTitle(row['Name'])
+        #TODO: Replace age with average for None
         if title == 'None':
             continue
-        average = titlesAverages[title]
-        row['Age'] = average
+        else:
+            average = titlesAverages[title]
+            df.set_value(index, 'Age', average)
 df.to_csv("train.csv")
